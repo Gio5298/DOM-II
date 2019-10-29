7b8e1ed97728e374110b5fcf2606467805acf62d
Regino P <regino5298@gmail.com>
2019-10-29 15:24:57 -0700

    added mouseover

--- js/index.js
+++ js/index.js
@@ -1,16 +1,35 @@
 // Your code goes here
+
+    //dblclick vvv
 const header = document.querySelector('.logo-heading');
     header.addEventListener('dblclick', function(event) {
         header.style.color='skyblue'
-    })
+    });
 
-    const yellowBus = document.querySelector('.yellow-bus-header');
+    //mouseenter vvv
+const yellowBus = document.querySelector('.yellow-bus-header');
     yellowBus.addEventListener('mouseenter', function(event){
         event.target.style.border = '3px solid moccasin';
         event.target.style.transform ='scale(1.1)';
         event.target.style.transition ='all 0.5s';
-
     });
+const nav = document.querySelectorAll('.nav a').forEach(el =>{
+    el.addEventListener('mouseenter', () =>{
+        el.style.color='red';
+        setTimeout(function(){
+            el.style.color='';
+        }, 400)
+    }, false);
+    //mouseover vvv
+    el.addEventListener('mouseover', () =>{
+        el.style.color='blue';
+        setTimeout(function(){
+            el.style.color='blue';
+        }, 350)
+    }), false;
+});
+
+    //mouseleave vvv
     yellowBus.addEventListener('mouseleave', function(event){
         event.target.style.border = '3px solid dodgerblue';
         event.target.style.transform ='scale(1)';

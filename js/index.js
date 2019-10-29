c226f8a01b4b2b2cb5e8682395699ed21733f3a8
Regino P <regino5298@gmail.com>
2019-10-29 14:38:42 -0700

    added mouseenter and mouseleave

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
 // Your code goes here
 const header = document.querySelector('.logo-heading');
-    //console.log(header);
     header.addEventListener('dblclick', function(event) {
         header.style.color='skyblue'
     })
+
+    const yellowBus = document.querySelector('.yellow-bus-header');
+    yellowBus.addEventListener('mouseenter', function(event){
+        event.target.style.border = '3px solid moccasin';
+        event.target.style.transform ='scale(1.1)';
+        event.target.style.transition ='all 0.5s';
+
+    });
+    yellowBus.addEventListener('mouseleave', function(event){
+        event.target.style.border = '3px solid dodgerblue';
+        event.target.style.transform ='scale(1)';
+        event.target.style.transition ='all 0.5s';
+    });

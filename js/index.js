c0dd220ddfa2a06a93a09ef920e2ce9bc3a6d185
Regino P <regino5298@gmail.com>
2019-10-29 14:14:07 -0700

    added dblclick

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+const header = document.querySelector('.logo-heading');
+    //console.log(header);
+    header.addEventListener('dblclick', function(event) {
+        header.style.color='skyblue'
+    })
